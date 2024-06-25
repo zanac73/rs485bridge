c904b5f6c7af462644ec201483683cbf21277350
Cristiano Brudna <cristiano.brudna@gmail.com>
2024-06-25 16:05:56 -0300

    feat: Initial implementation

---
+++ readme.md
@@ -0,0 +1,36 @@
+# RS485 Bridge
+
+This code is an Arduino sketch that creates a simple RS485 bridge. 
+
+This code creates a simple RS485 bridge that allows a PC to communicate with a device, for instance a sensor, using the RS485 protocol through the USB serial communication.
+
+```mermaid
+graph LR
+    PC((PC)) <-- USB --> Arduino[Arduino Uno]
+    Arduino <--> RS485_Module[RS485 module]
+    RS485_Module <-- RS485 --> Sensor((Sensor))
+```
+
+The code includes the following features:
+- It uses the SoftwareSerial library to create a serial communication interface for the RS485 bus.
+- The baud rate for both the USB serial communication and the RS485 communication is defined as 4800.
+
+# Supported Boards
+
+**Arduino AVR**
+
+# Pins
+| RS485 module           | Arduino Pin |
+|:------------------|:------------|
+| DE (Data Enable) | 3           |
+| RE (Receive Enable) | 4           |
+| DI | 5      |
+| RO | 2      |
+
+# Code Explanation
+- In the `setup()` function, the USB serial communication is initialized, the control pins for the RS485 are set as outputs, and the RS485 serial communication is initialized.
+- The `setRs485ReceiveMode()` function sets the DE and RE pins to LOW, enabling the RS485 receiver.
+- The `setRs485TransmitMode()` function sets the DE and RE pins to HIGH, enabling the RS485 transmitter.
+- In the `loop()` function, the code checks if data is available on the RS485 bus. If data is available, it reads the data and sends it to the USB serial communication.
+- The code also checks if data is available on the USB serial communication. If data is available, it enables the RS485 transmit mode, reads the data, and sends it to the RS485 bus. After sending the data, the RS485 receiver mode is enabled again.
+
---
+++ rs485bridge.ino
@@ -0,0 +1,83 @@
+/**
+ * @file rs485bridge.ino
+ * @author Cristiano Brudna (cristiano.brudna@gmail.com)
+ * @date 2024-06-25
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <SoftwareSerial.h>
+
+#define BAUD_RATE                       4800
+
+#define SERIAL_BAUD_RATE                BAUD_RATE
+
+#define RS485_BAUD_RATE                 BAUD_RATE
+#define RS485_DE_PIN                    3
+#define RS485_RE_PIN                    4
+#define RS485_RX_PIN                    5  // DI
+#define RS485_TX_PIN                    2  // RO
+
+SoftwareSerial rs485Serial(RS485_RX_PIN, RS485_TX_PIN); // RX, TX
+
+
+void setRs485ReceiveMode()
+{
+    digitalWrite(RS485_DE_PIN, LOW);
+    digitalWrite(RS485_RE_PIN, LOW);
+}
+
+void setRs485TransmitMode()
+{
+    digitalWrite(RS485_DE_PIN, HIGH);
+    digitalWrite(RS485_RE_PIN, HIGH);
+}
+
+void setup()
+{
+    // Initialize Serial for USB communication
+    Serial.begin(SERIAL_BAUD_RATE);
+    while(!Serial)
+    {
+        delay(10);
+    }
+
+    // Initialize the control pins
+    pinMode(RS485_DE_PIN, OUTPUT);
+    pinMode(RS485_RE_PIN, OUTPUT);
+    
+    setRs485ReceiveMode();
+
+    // Initialize SoftwareSerial for RS485 communication
+    rs485Serial.begin(RS485_BAUD_RATE);
+}
+
+void loop()
+{
+    // Check if data is available on the RS485 bus
+    if (rs485Serial.available())
+    {
+        // Read data from the RS485 bus and send it to the Serial (USB)
+        while (rs485Serial.available())
+        {
+            char c = rs485Serial.read();
+            Serial.write(c);
+        }
+    }
+
+    // Check if data is available on the Serial (USB)
+    if (Serial.available())
+    {
+        setRs485TransmitMode();
+
+        // Read data from the Serial (USB) and send it to the RS485 bus
+        while (Serial.available())
+        {
+            char c = Serial.read();
+            rs485Serial.write(c);
+        }
+
+        setRs485ReceiveMode();
+    }
+}
